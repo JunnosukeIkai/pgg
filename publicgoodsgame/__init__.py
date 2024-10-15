68be6144d397e1466b5bd126d3a0c0d534688f5c
JunnosukeIkai <ec1007hp@ed.ritsumei.ac.jp>
2024-10-15 13:15:56 +0900

    pgg

--- publicgoodsgame/__init__.py
+++ publicgoodsgame/__init__.py
@@ -9,9 +9,9 @@ Your app description
 class C(BaseConstants):
     NAME_IN_URL = 'publicgoodsgame'
     PLAYERS_PER_GROUP = 4
-    NUM_ROUNDS = 10
-    ENDOWMENT = 500
-    MULTIPLIER = 2
+    NUM_ROUNDS = 5
+    ENDOWMENT = 10
+    MULTIPLIER = 1.6
 
 
 class Subsession(BaseSubsession):
@@ -29,17 +29,8 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-   contribution = models.IntegerField(min=0)
-   prev_payoff = models.CurrencyField() 
+   contribution = models.IntegerField(min=0, max=10)
    total_payoff = models.CurrencyField()
-   payoff_pre = models.CurrencyField()
-
-def contribution_max(player):
-    if player.round_number == 1:
-        return C.ENDOWMENT
-    else:
-        prev_payoff  =  player.in_round(player.round_number - 1).payoff
-        return prev_payoff
 
 
 def set_payoff(group):
@@ -47,20 +38,13 @@ def set_payoff(group):
     contributions = [p.contribution for p in players]
     group.total_contribution = sum(contributions)
     group.individual_share = group.total_contribution * C.MULTIPLIER / C.PLAYERS_PER_GROUP
-    for player in players:
-        if group.round_number == 1:
-            player.payoff = C.ENDOWMENT - player.contribution + group.individual_share
-        elif group.round_number == 5:
-            player.prev_payoff  =  player.in_round(player.round_number - 1).payoff
-            player.payoff_pre = (player.prev_payoff - player.contribution + group.individual_share)
-            player.payoff = (player.prev_payoff - player.contribution + group.individual_share)*0.3
-            player.total_payoff = player.prev_payoff + player.payoff
+    for p in players :
+        if group.round_number == 5:
+            p.payoff = C.ENDOWMENT - p.contribution + group.individual_share
+            p.total_payoff = p.payoff + p.in_round(p.round_number-1).payoff + p.in_round(p.round_number-2).payoff + p.in_round(p.round_number-3).payoff + p.in_round(p.round_number-4).payoff           
         else:
-            player.prev_payoff  =  player.in_round(player.round_number - 1).payoff
-            player.payoff = player.prev_payoff - player.contribution + group.individual_share
-            player.total_payoff = player.prev_payoff + player.payoff
+            p.payoff = C.ENDOWMENT - p.contribution + group.individual_share
 
-# PAGES
 
 class Instraction(Page):
     def is_displayed(player: Player):
@@ -72,72 +56,21 @@ class Contribute(Page):
     form_fields = ['contribution']
 
 
-    def is_displayed(player: Player):
-        return player.round_number == 1
-
-
-class Contribute2(Page):
-    form_model = 'player'
-    form_fields = ['contribution']
-
-    @staticmethod 
-    def vars_for_template(player: Player): 
-        rval = {} 
-        rval['prev_payoff'] =  player.in_round(player.round_number - 1).payoff
-        return rval
-
-    def is_displayed(player: Player):
-        return player.round_number != 1
-
-
 class ResultsWaitPage(WaitPage):
     after_all_players_arrive = 'set_payoff'
 
 
-class Catastrophe(Page):
-    def is_displayed(player: Player):
-        return player.round_number == 5
-
-
 class Results(Page):
     def is_displayed(player: Player):
-        return player.round_number == 1
+        return player.round_number != 5
 
-
-class Results2(Page):
-    def is_displayed(player: Player):
-        return (player.round_number != 1) & (player.round_number != 5)
-
-
-    @staticmethod 
-    def vars_for_template(player: Player): 
-        rval = {} 
-        rval['prev_payoff'] =  player.in_round(player.round_number - 1).payoff
-        return rval
-
-
-class Results3(Page):
+class Results5(Page):
     def is_displayed(player: Player):
         return player.round_number == 5
-
-
-    @staticmethod 
-    def vars_for_template(player: Player): 
-        rval = {} 
-        rval['prev_payoff'] =  player.in_round(player.round_number - 1).payoff
-        return rval
-
-
-
-
-
 
 
 page_sequence = [Instraction, 
                   Contribute,
-                  Contribute2,
                   ResultsWaitPage,
                   Results,
-                  Results2,
-                  Results3,
-                  Catastrophe,]
+                  Results5]
